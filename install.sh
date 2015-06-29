27ea6325807a4b9894ac58c15700632afda24ef2
Tyler Ball <tyler@tylerball.net>
2015-06-29 19:27:52 +0000

    ansible: support linux dev boxes

--- install.sh
+++ install.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
-if [[ -x `which brew` ]]; then
-  brew update 2>&1
-  brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup 2>&1
-  brew doctor 2>&1
-else
-  echo "Installing homebrew"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew doctor
-fi
+if [[ "$(uname)" = 'Darwin' ]]; then
+  if [[ -x `which brew` ]]; then
+    brew update 2>&1
+    brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup 2>&1
+    brew doctor 2>&1
+  else
+    echo "Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew doctor
+  fi
 
-if [[ ! -x `which ansible` ]]; then
-  echo "Installing ansible"
-  brew install ansible 2>&1
+  if [[ ! -x `which ansible` ]]; then
+    echo "Installing ansible"
+    brew install ansible 2>&1
+  fi
+else
+  if [[ ! -x `which ansible` ]]; then
+    echo "Installing ansible"
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install ansible
+  fi
 fi
 
 ansible-playbook site.yml
