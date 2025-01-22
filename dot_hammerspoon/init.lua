889fbd1f7ab569951df7e378543ad6d7adbbeb13
Tyler Ball <tyler@tylerball.net>
2025-01-22 09:53:35 -0500

    begin moving to raycast

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,19 +1,6 @@
-require 'settings'
 require 'layout'
-require 'apps'
-require 'itunes'
-require 'music'
-require 'caffeine'
 
 hs.loadSpoon('ControlEscape'):start()
-local VimMode = hs.loadSpoon('VimMode')
-local vim = VimMode:new()
-vim
-  :disableForApp('iTerm')
-  :disableForApp('Screen Sharing')
-  :shouldDimScreenInNormalMode(false)
-  :enterWithSequence('kj')
-
 hs.settings.set('modifier', {'cmd', 'shift', 'ctrl'})
 
 hs.pathwatcher.new(hs.configdir .. '/', hs.reload):start()
@@ -24,9 +11,3 @@ hs.audiodevice.watcher.setCallback(function ()
   end
 end)
 hs.audiodevice.watcher.start()
-
-local ctrlDoublePress = require('doubletap')
-ctrlDoublePress.timeFrame = 2
-ctrlDoublePress.action = function()
-  vim:setInsertMode()
-end
--- dot_hammerspoon/settings.lua
+++
@@ -1,6 +0,0 @@
-hs.settings.set('modifier', {'cmd', 'shift', 'ctrl'})
-hs.settings.set('NightShift:ignoredApps', {
-  'Lightroom',
-  'Adobe Premiere Pro CC 2015',
-})
-hs.settings.set('NightShift:strength', 50)
